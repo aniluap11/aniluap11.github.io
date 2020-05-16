871ae84d14a21bfcf7ae4206fb984528aa5b28b1
aniluap11 <62618349+aniluap11@users.noreply.github.com>
2020-05-16 14:32:25 -0500

    Update slimbox2.js

--- js/slimbox2.js
+++ js/slimbox2.js
@@ -8,7 +8,12 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
 	jQuery(function($) {
-		$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
+		$("a[rel^='lightbox']").slimbox({overlayOpacity: 0.6,
+	resizeEasing: "easeOutElastic",
+	captionAnimationDuration: 1,
+	counterText: "Image {x} sur {y}",
+	closeKeys: [27, 70],
+	nextKeys: [39, 83]}, null, function(el) {
 			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 		});
 	});
